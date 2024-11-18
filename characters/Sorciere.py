cf2d5f1d96cba5ad17d3afe81586bb346efef649
adeleB <adele.bernet05@gmail.com>
2024-11-18 17:21:48 +0100

    self.text_role et self.text_info

--- characters/Sorciere.py
+++ characters/Sorciere.py
@@ -6,8 +6,8 @@ class Sorciere(Villageois):
         self.name = "Sorcière"
         self.alt_name = "Troll"
         self.card = "./assets/images/cards/sorciere_carte.jpeg"
-        self.texte_role = "Elle a 2 potions lui permettant de sauver un joueur attaqué par les loups-garous ou d’en éliminer un."
-        self.texte_info = "Tiré du film La Reine des Neiges"
+        self.text_role = "Elle a 2 potions lui permettant de sauver un joueur attaqué par les loups-garous ou d’en éliminer un."
+        self.text_info = "Tiré du film La Reine des Neiges"
 
     def night_action(self):
         return
